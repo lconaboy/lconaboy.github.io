fa992181fb4581429f7552eabdfa23c7f28d855c
Luke Conaboy <lukeconaboy@gmail.com>
2025-05-19 16:41:14 +0100

    auto update

--- bib/strip_bib.py
+++ bib/strip_bib.py
@@ -1,3 +1,8 @@
+"""
+This is the worst piece of code I have ever written.
+"""
+import os
+import re
 import sys
 
 inp_fn = sys.argv[1]
@@ -42,19 +47,88 @@ out.write('	    <p>\n')
 
 # Now the actual bibliography
 write = True
+new_entry = False
+end_entry = False
+author_key = None
+bib_entry = {}
+old_lini = None
+
+# FIXME bad stuff happens with multi-line author lists if an initial is repeated
+
+if write: out.write('<table>\n')
+
 for l in inp:
-    if l[0:8] == "</table>":
+    if l[0] in ['<', '[', ']']:
+        lini = l[0:5]
+    else:
+        lini = l
+        
+    if lini == "</tab":
         print("-- stopping writing")
         write = False
         out.write("</table>\n")
-    else:
-        if write: out.write(l)
+        break
+    elif new_entry:
+        bib_entry[lini] = l
+        if lini == '</tr>':
+            new_entry = False
+            end_entry = True
+    elif lini == '<tr v':
+        new_entry = True
+        bib_entry[lini] = l
+    old_lini = lini
+
+    # We have reached the end of this bibtex entry, so now we can play
+    # around with it before writing it out
+    if end_entry:
+        # Find out the citekey associated with this entry
+        # print(bib_entry['[<a n'])
+        match = re.search('name="(\w+\d+\w*)"', bib_entry['[<a n'])
+        name = match.group(1)
+        # print(name)
+
+        # If I have uploaded the pdf, link to it
+        pdf_file = f'../pdf/{name:s}.pdf'
+        if os.path.isfile(pdf_file):
+            pdf = f'<a href="pdf/{name:s}.pdf">pdf</a>&nbsp;|&nbsp;'
+        else:
+            pdf = ''
+            print(pdf_file, 'not found')
+
+        # This key might not have a link to the arXiv/DOI version (somehow)
+        try:
+            hrefs = bib_entry['[&nbs'] + pdf + bib_entry['<a hr']+'\n'
+        except KeyError as e:
+            hrefs = bib_entry['[&nbs'][:-1] + pdf + '\n'
+
+        # Remove [] from links
+        hrefs_stripped = ''
+        for x in hrefs:
+            if x not in ['[', ']', '|']:
+                hrefs_stripped += x
+        hrefs = hrefs_stripped
+
+        # Combine all the links together to be placed in the first cell
+        bib_entry['[&nbs'] = ''
+        bib_entry['<a hr'] = ''
+        bib_entry['[<a n'] = f'<a name={name:s}>{hrefs:s}</a>\n'
+
+        # Finally write out the entire entry
+        if write:
+            for line_key, bib_line in bib_entry.items():
+                out.write(bib_line)
+            out.write('</td>\n')  # this tag is not stored, because there is one earlier
+            
+        bib_entry = {}
+        end_entry = False
+        # if write: out.write(l)
+        
 
 # Finish off the second half
 out.write('	    </p>\n')
 out.write('\n')
 out.write('	  <p style="font-size: 14px">\n')
-out.write('	     Bibliography converted from bibtex to HTML using <a href="http://www.lri.fr/~filliatr/bibtex2html/"><tt>bibtex2html</tt></a>\n')
+out.write('	     Bibliography converted from bibtex to HTML using <a href="http://www.lri.fr/~filliatr/bibtex2html/"><tt>bibtex2html</tt></a>, inspired by <a href=https://fleuret.org/francois/publications.html> François Fleuret </a>\n')
 out.write('	  </p>\n')
 
 out.write('	  </div>\n')
